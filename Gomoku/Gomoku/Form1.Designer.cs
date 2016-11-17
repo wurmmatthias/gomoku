c41c819e738b0261548ff48a939e53be5ea293a1
M.Wurm0706 <M.Wurm0706@PC-D307-16>
2016-11-17 13:17:18 +0100

    Initial

---
+++ Gomoku/Gomoku/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Gomoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(345, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Spielen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(345, 406);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(252, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Zurück zum Desktop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Bradley Hand ITC", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(277, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 120);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gomoku";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(345, 339);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(252, 61);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Hilfe";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(928, 644);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gomoku";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Gomoku/Gomoku/Form2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gomoku
+{
+    public partial class Form2 : Form
+    {
+        private System.Windows.Forms.Button[] _Buttons;
+        public int playCounter = 1;
+        public Form2()
+        {
+            InitializeComponent();
+            initializeField();
+        }
+
+
+        private void initializeField()
+        {
+
+            this.SuspendLayout();
+
+            int x = 0;
+            int y = 0;
+
+            //Magic happens here ;)
+            _Buttons = new Button[324];
+
+            for (int index = 0; index < 324; index++)
+            {
+
+                createButton(x, y, index);
+
+
+                if (x < 17)
+                {
+                    x++;
+                }
+                else
+                {
+                    x = 0;
+                    y++;
+                }
+
+            }
+
+            this.ResumeLayout();
+
+        }
+
+        private void createButton(int X_, int Y_, int Index_)
+        {
+            this._Buttons[Index_] = new System.Windows.Forms.Button();
+
+            this._Buttons[Index_].Location = new System.Drawing.Point(X_ * 30, Y_ * 30);
+            this._Buttons[Index_].Name = "button" + Index_.ToString();
+            this._Buttons[Index_].Size = new System.Drawing.Size(30, 30);
+            this._Buttons[Index_].TabIndex = 0;
+            this._Buttons[Index_].UseVisualStyleBackColor = true;
+            _Buttons[Index_].Click += (sender1, ex) => this.ColorAdding(Index_);
+
+            this.Controls.Add(this._Buttons[Index_]);
+
+        }
+
+        public void ColorAdding(int Index_)
+        {
+            playCounter += 1;
+            if (playCounter % 2 == 0)
+            {
+                _Buttons[Index_].BackColor = Color.Red;
+                _Buttons[Index_].Enabled = false;
+                this.Text = "Gomoku - Spiel (Spieler 2 ist am Zug.)";
+            } else
+            {
+                _Buttons[Index_].BackColor = Color.Blue;
+                _Buttons[Index_].Enabled = false;
+                this.Text = "Gomoku - Spiel (Spieler 1 ist am Zug.)";
+            }
+        }
+
+    }
+}
