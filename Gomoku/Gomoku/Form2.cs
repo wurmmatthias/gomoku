149ba3261b90406b8a68c04315b2f8601b1fa0a3
wurmm <wurmm@MATTHIAS-PC>
2016-11-22 22:13:59 +0100

    Game Logic

--- Gomoku/Gomoku/Form2.cs
+++ Gomoku/Gomoku/Form2.cs
@@ -56,13 +56,12 @@ namespace Gomoku
             this._Buttons[Index_].Size = new System.Drawing.Size(30, 30);
             this._Buttons[Index_].TabIndex = 0;
             this._Buttons[Index_].UseVisualStyleBackColor = true;
-            _Buttons[Index_].Click += (sender1, ex) => this.ColorAdding(Index_);
+            _Buttons[Index_].Click += (sender1, ex) => this.OnButtonClick(Index_);
 
             this.Controls.Add(this._Buttons[Index_]);
-
         }
 
-        public void ColorAdding(int Index_)
+        public void OnButtonClick(int Index_)
         {
             playCounter += 1;
             if (playCounter % 2 == 0)
@@ -75,6 +74,55 @@ namespace Gomoku
                 _Buttons[Index_].Enabled = false;
                 this.Text = "Gomoku - Spiel (Spieler 1 ist am Zug.)";
             }
+
+            // Horizontal-Check
+            if ((_Buttons[Index_].BackColor == Color.Red && _Buttons[Index_ - 1].BackColor == Color.Red && _Buttons[Index_ - 2].BackColor == Color.Red && _Buttons[Index_ - 3].BackColor == Color.Red && _Buttons[Index_ - 4].BackColor == Color.Red) || (_Buttons[Index_].BackColor == Color.Red && _Buttons[Index_ + 1].BackColor == Color.Red && _Buttons[Index_ + 2].BackColor == Color.Red && _Buttons[Index_ + 3].BackColor == Color.Red && _Buttons[Index_ + 4].BackColor == Color.Red))
+            {
+                MessageBox.Show("Spieler 1 hat gewonnen!", "Gewonnen!");
+                this.Close();
+            }
+            if ((_Buttons[Index_].BackColor == Color.Blue && _Buttons[Index_-1].BackColor == Color.Blue && _Buttons[Index_ - 2].BackColor == Color.Blue && _Buttons[Index_ - 3].BackColor == Color.Blue && _Buttons[Index_ - 4].BackColor == Color.Blue) || (_Buttons[Index_].BackColor == Color.Blue && _Buttons[Index_ + 1].BackColor == Color.Blue && _Buttons[Index_ + 2].BackColor == Color.Blue && _Buttons[Index_ + 3].BackColor == Color.Blue && _Buttons[Index_ + 4].BackColor == Color.Blue))
+            {
+                MessageBox.Show("Spieler 2 hat gewonnen!","Gewonnen!");
+                this.Close();
+            }
+
+            // Vertikal-Check
+            if ((_Buttons[Index_].BackColor == Color.Red && _Buttons[Index_ - 18].BackColor == Color.Red && _Buttons[Index_ - 36].BackColor == Color.Red && _Buttons[Index_ - 54].BackColor == Color.Red && _Buttons[Index_ - 72].BackColor == Color.Red) || (_Buttons[Index_].BackColor == Color.Red && _Buttons[Index_ + 18].BackColor == Color.Red && _Buttons[Index_ + 36].BackColor == Color.Red && _Buttons[Index_ + 54].BackColor == Color.Red && _Buttons[Index_ + 72].BackColor == Color.Red))
+            {
+                MessageBox.Show("Spieler 1 hat gewonnen!", "Gewonnen!");
+                this.Close();
+            }
+            if ((_Buttons[Index_].BackColor == Color.Blue && _Buttons[Index_ - 18].BackColor == Color.Blue && _Buttons[Index_ - 36].BackColor == Color.Blue && _Buttons[Index_ - 54].BackColor == Color.Blue && _Buttons[Index_ - 72].BackColor == Color.Blue) || (_Buttons[Index_].BackColor == Color.Blue && _Buttons[Index_ + 18].BackColor == Color.Blue && _Buttons[Index_ + 36].BackColor == Color.Blue && _Buttons[Index_ + 54].BackColor == Color.Blue && _Buttons[Index_ + 72].BackColor == Color.Blue))
+            {
+                MessageBox.Show("Spieler 2 hat gewonnen!", "Gewonnen!");
+                this.Close();
+            }
+
+            // Quer-Check
+            //Rechts
+            if ((_Buttons[Index_].BackColor == Color.Red && _Buttons[Index_ - 17].BackColor == Color.Red && _Buttons[Index_ - 34].BackColor == Color.Red && _Buttons[Index_ - 51].BackColor == Color.Red && _Buttons[Index_ - 68].BackColor == Color.Red) || (_Buttons[Index_].BackColor == Color.Red && _Buttons[Index_ + 17].BackColor == Color.Red && _Buttons[Index_ + 34].BackColor == Color.Red && _Buttons[Index_ + 51].BackColor == Color.Red && _Buttons[Index_ + 68].BackColor == Color.Red))
+            {
+                MessageBox.Show("Spieler 1 hat gewonnen!", "Gewonnen!");
+                this.Close();
+            }
+            if ((_Buttons[Index_].BackColor == Color.Blue && _Buttons[Index_ - 17].BackColor == Color.Blue && _Buttons[Index_ - 34].BackColor == Color.Blue && _Buttons[Index_ - 51].BackColor == Color.Blue && _Buttons[Index_ - 68].BackColor == Color.Blue) || (_Buttons[Index_].BackColor == Color.Blue && _Buttons[Index_ + 17].BackColor == Color.Blue && _Buttons[Index_ + 34].BackColor == Color.Blue && _Buttons[Index_ + 51].BackColor == Color.Blue && _Buttons[Index_ + 68].BackColor == Color.Blue))
+            {
+                MessageBox.Show("Spieler 2 hat gewonnen!", "Gewonnen!");
+                this.Close();
+            }
+            //Links
+            if ((_Buttons[Index_].BackColor == Color.Red && _Buttons[Index_ - 19].BackColor == Color.Red && _Buttons[Index_ - 38].BackColor == Color.Red && _Buttons[Index_ - 57].BackColor == Color.Red && _Buttons[Index_ - 76].BackColor == Color.Red) || (_Buttons[Index_].BackColor == Color.Red && _Buttons[Index_ + 19].BackColor == Color.Red && _Buttons[Index_ + 38].BackColor == Color.Red && _Buttons[Index_ + 57].BackColor == Color.Red && _Buttons[Index_ + 76].BackColor == Color.Red))
+            {
+                MessageBox.Show("Spieler 1 hat gewonnen!", "Gewonnen!");
+                this.Close();
+            }
+            if ((_Buttons[Index_].BackColor == Color.Blue && _Buttons[Index_ - 19].BackColor == Color.Blue && _Buttons[Index_ - 38].BackColor == Color.Blue && _Buttons[Index_ - 57].BackColor == Color.Blue && _Buttons[Index_ - 76].BackColor == Color.Blue) || (_Buttons[Index_].BackColor == Color.Blue && _Buttons[Index_ + 19].BackColor == Color.Blue && _Buttons[Index_ + 38].BackColor == Color.Blue && _Buttons[Index_ + 57].BackColor == Color.Blue && _Buttons[Index_ + 76].BackColor == Color.Blue))
+            {
+                MessageBox.Show("Spieler 2 hat gewonnen!", "Gewonnen!");
+                this.Close();
+            }
+
         }
 
     }
